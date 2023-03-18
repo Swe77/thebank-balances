76ae0b0411c615abd1684bf3a632e36cbd469b1d
Odante Swerts <odante.swerts@hotmail.com>
2023-03-17 21:03:27 -0300

    oidc included

--- src/main/kotlin/org/acme/BalanceResource.kt
+++ src/main/kotlin/org/acme/BalanceResource.kt
@@ -4,6 +4,7 @@ import org.acme.dto.BalanceDTO
 import java.math.BigDecimal
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
@@ -12,8 +13,8 @@ class BalanceResource {
 
     @GET
     @Path("/account/{id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    fun getBalance(id: Long) : BalanceDTO {
+    @Produces(MediaType.APPLICATION_JSON)
+    suspend fun getBalance(@PathParam("id") id: Long) : BalanceDTO {
         return BalanceDTO(1L, id, BigDecimal.valueOf(50 + id))
 
     }
--- src/main/kotlin/org/acme/dto/BalanceDTO.kt
+++ src/main/kotlin/org/acme/dto/BalanceDTO.kt
@@ -2,4 +2,4 @@ package org.acme.dto
 
 import java.math.BigDecimal
 
-data class BalanceDTO(val id: Long, val accountId: Long, val balance: BigDecimal)
+data class BalanceDTO(val id: Long, val accountId: Long, val amount: BigDecimal)
